bc377d03df2747f2813d7a60a69d7cecb4d20d46
Sheng Huan Lee <shenghuan.lee@ecloudvaley.com>
2025-03-25 16:06:11 +0800

    Yang Guang Cai Hong Xiao Bai MA Update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bello!From NigaNigaNigaNiga'));
+app.get('/', (req, res) => res.send('Yang Guang Cai Hong Xiao bai Ma'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
